156cd2229ab94fb1fba0d5e520f2f99ba3e74234
Matthew Carroll <matt@fail-harder.com>
2014-03-15 18:25:02 -0400

    sendgrid try 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,23 @@ Pinteresting::Application.configure do
   
   # Required for Heroku
   config.action_mailer.default_url_options = { :host => 'http://fhpinapp.herokuapp.com/' }
-  
+  # ActionMailer Config
+	# Setup for production - deliveries, no errors raised
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.perform_deliveries = true		
+	config.action_mailer.raise_delivery_errors = false
+	config.action_mailer.default :charset => "utf-8"
+	
+	ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+	}
+
   # Sets Paperclip to upload images to Amazon S3
   config.paperclip_defaults = {
     :storage => :s3,
